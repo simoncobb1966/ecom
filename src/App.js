56ef7846c9221ca1f729b72536efe749f8e6acfb
simoncobb1966 <simoncobb1966@gmail.com>
2019-05-03 13:21:40 +0100

    got a modal & buttons working with material-ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Modalbutton from './components/modalbutton';
+import Header from './components/header';
+import Footer from './components/footer';
+import Button from '@material-ui/core/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  buttonHandler = (buttonName) => {
+    alert("Button handler " + buttonName.currentTarget.value)
+  }
+
+  render() {
+    return (
+
+      <div className="outerdiv">
+        <div class="container">
+          <div className="maindiv">
+            <Header />
+            <div className="row">
+              <div class="col-sm">
+              </div>
+              <div class="col-sm">
+                <Modalbutton
+                  buttonHandlerFunction={this.buttonHandler}
+                />
+              </div>
+              <div class="col-sm">
+              <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+              </div>
+
+            </div>
+            <Footer />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+class Header extends Component {
+
+
+    render() {
+        return (
+
+            <div>
+                <div class="container">
+
+                    <div className="row">
+                        <div class="col-sm header">
+                            <h1>James Bond Shop</h1>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Header;
